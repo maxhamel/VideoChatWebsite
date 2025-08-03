f01f3c956752167f17925a31c928c7a047b3f9fa
Max Hamel <maxahamel@gmail.com>
2025-08-03 17:43:38 -0400

    functional webrtc video calling

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,43 @@
-localVideoEl = document.querySelector("#local-video");
+const localVideoEl = document.querySelector("#local-video");
+const remoteVideoEl = document.querySelector("#remote-video");
+
+let localStream;
+let remoteStream;
+let peerConnection;
+let didIOffer = false;
+const userName = "Max-" + Math.floor(Math.random() * 100000);
+const password = "x";
+const socket = io.connect("https://localhost:3000/", {
+  auth: {
+    userName,
+    password,
+  },
+});
+
+let peerConfiguration = {
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
+    },
+  ],
+};
+
+const call = async (e) => {
+  await fetchUserMedia();
+
+  await createPeerConnection();
+
+  try {
+    console.log("Creating offer...");
+    const offer = await peerConnection.createOffer();
+    console.log(offer);
+    peerConnection.setLocalDescription(offer);
+    didIOffer = true;
+    socket.emit("newOffer", offer);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const fetchUserMedia = () => {
   return new Promise(async (resolve, reject) => {
@@ -8,6 +47,7 @@ const fetchUserMedia = () => {
         audio: true,
       });
       localVideoEl.srcObject = stream;
+      localStream = stream;
       resolve();
     } catch (err) {
       console.log(err);
@@ -16,4 +56,102 @@ const fetchUserMedia = () => {
   });
 };
 
-document.querySelector("#call").addEventListener("click", fetchUserMedia);
+const createPeerConnection = (offerObj) => {
+  return new Promise(async (resolve, reject) => {
+    peerConnection = await new RTCPeerConnection(peerConfiguration);
+    remoteStream = new MediaStream();
+    remoteVideoEl.srcObject = remoteStream;
+
+    localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, localStream);
+    });
+
+    peerConnection.addEventListener("signalingstatechange", (event) => {
+      console.log(event);
+      console.log(peerConnection.signalingState);
+    });
+
+    peerConnection.addEventListener("icecandidate", (e) => {
+      console.log("........Ice candidate found!......");
+      console.log(e);
+      if (e.candidate) {
+        socket.emit("sendIceCandidateToSignalingServer", {
+          iceCandidate: e.candidate,
+          iceUserName: userName,
+          didIOffer,
+        });
+      }
+    });
+
+    peerConnection.addEventListener("track", (e) => {
+      console.log("Got a track from the other peer");
+      console.log(e);
+      e.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track, remoteStream);
+      });
+    });
+
+    if (offerObj) {
+      await peerConnection.setRemoteDescription(offerObj.offer);
+    }
+    resolve();
+  });
+};
+
+const addNewIceCandidate = (iceCandidate) => {
+  peerConnection.addIceCandidate(iceCandidate);
+  console.log("======Added Ice Candidate======");
+};
+
+socket.on("availableOffers", (offers) => {
+  console.log(offers);
+  createOfferEls(offers);
+});
+
+socket.on("newOfferAwaiting", (offers) => {
+  createOfferEls(offers);
+});
+
+socket.on("answerResponse", (offerObj) => {
+  console.log(offerObj);
+  addAnswer(offerObj);
+});
+
+socket.on("receivedIceCandidateFromServer", (iceCandidate) => {
+  addNewIceCandidate(iceCandidate);
+  console.log(iceCandidate);
+});
+
+function createOfferEls(offers) {
+  const answerEl = document.querySelector("#answer");
+  offers.forEach((o) => {
+    console.log(o);
+    const newOfferEl = document.createElement("div");
+    newOfferEl.innerHTML = `<button class="btn btn-success col-1">Answer ${o.offererUserName}</button>`;
+    newOfferEl.addEventListener("click", () => answerOffer(o));
+    answerEl.appendChild(newOfferEl);
+  });
+}
+const answerOffer = async (offerObj) => {
+  await fetchUserMedia();
+  await createPeerConnection(offerObj);
+  const answer = await peerConnection.createAnswer({});
+  await peerConnection.setLocalDescription(answer);
+  console.log(offerObj);
+  console.log(answer);
+
+  offerObj.answer = answer;
+
+  const offerIceCandidates = await socket.emitWithAck("newAnswer", offerObj);
+  offerIceCandidates.forEach((c) => {
+    peerConnection.addIceCandidate(c);
+    console.log("======Added Ice Candidate======");
+  });
+  console.log(offerIceCandidates);
+};
+
+const addAnswer = async (offerObj) => {
+  await peerConnection.setRemoteDescription(offerObj.answer);
+};
+
+document.querySelector("#call").addEventListener("click", call);
--- server.js
+++ server.js
@@ -10,14 +10,158 @@ const bcrypt = require("bcrypt");
 const UsersDAO = require("./dao/usersDAO.js");
 const User = require("./models/user");
 const path = require("path");
+const socketio = require("socket.io");
+const https = require("https");
 
+const key = fs.readFileSync("cert.key");
+const cert = fs.readFileSync("cert.crt");
 const app = express();
+const expressServer = https.createServer({ key, cert }, app);
+
+const io = socketio(expressServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(express.static("public"));
 app.use("/", indexRouter);
-app.listen(process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+expressServer.listen(process.env.PORT || 3000);
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const offers = [
+  // offererUserName
+  // offer
+  // offerIceCandidates
+  // answererUserName
+  // answer
+  // answererIceCandidates
+];
+const connectedSockets = [
+  //username, socketId
+];
+
+io.on("connection", (socket) => {
+  // console.log("Someone has connected");
+  const userName = socket.handshake.auth.userName;
+  const password = socket.handshake.auth.password;
+
+  if (password !== "x") {
+    socket.disconnect(true);
+    return;
+  }
+  connectedSockets.push({
+    socketId: socket.id,
+    userName,
+  });
+
+  //a new client has joined. If there are any offers available,
+  //emit them out
+  if (offers.length) {
+    socket.emit("availableOffers", offers);
+  }
+
+  socket.on("newOffer", (newOffer) => {
+    offers.push({
+      offererUserName: userName,
+      offer: newOffer,
+      offerIceCandidates: [],
+      answererUserName: null,
+      answer: null,
+      answererIceCandidates: [],
+    });
+    // console.log(newOffer.sdp.slice(50))
+    //send out to all connected sockets EXCEPT the caller
+    socket.broadcast.emit("newOfferAwaiting", offers.slice(-1));
+  });
+
+  socket.on("newAnswer", (offerObj, ackFunction) => {
+    console.log(offerObj);
+    //emit this answer (offerObj) back to CLIENT1
+    //in order to do that, we need CLIENT1's socketid
+    const socketToAnswer = connectedSockets.find(
+      (s) => s.userName === offerObj.offererUserName
+    );
+    if (!socketToAnswer) {
+      console.log("No matching socket");
+      return;
+    }
+    //we found the matching socket, so we can emit to it!
+    const socketIdToAnswer = socketToAnswer.socketId;
+    //we find the offer to update so we can emit it
+    const offerToUpdate = offers.find(
+      (o) => o.offererUserName === offerObj.offererUserName
+    );
+    if (!offerToUpdate) {
+      console.log("No OfferToUpdate");
+      return;
+    }
+    //send back to the answerer all the iceCandidates we have already collected
+    ackFunction(offerToUpdate.offerIceCandidates);
+    offerToUpdate.answer = offerObj.answer;
+    offerToUpdate.answererUserName = userName;
+    //socket has a .to() which allows emiting to a "room"
+    //every socket has it's own room
+    socket.to(socketIdToAnswer).emit("answerResponse", offerToUpdate);
+  });
+
+  socket.on("sendIceCandidateToSignalingServer", (iceCandidateObj) => {
+    const { didIOffer, iceUserName, iceCandidate } = iceCandidateObj;
+    // console.log(iceCandidate);
+    if (didIOffer) {
+      //this ice is coming from the offerer. Send to the answerer
+      const offerInOffers = offers.find(
+        (o) => o.offererUserName === iceUserName
+      );
+      if (offerInOffers) {
+        offerInOffers.offerIceCandidates.push(iceCandidate);
+        // 1. When the answerer answers, all existing ice candidates are sent
+        // 2. Any candidates that come in after the offer has been answered, will be passed through
+        if (offerInOffers.answererUserName) {
+          //pass it through to the other socket
+          const socketToSendTo = connectedSockets.find(
+            (s) => s.userName === offerInOffers.answererUserName
+          );
+          if (socketToSendTo) {
+            socket
+              .to(socketToSendTo.socketId)
+              .emit("receivedIceCandidateFromServer", iceCandidate);
+          } else {
+            console.log("Ice candidate recieved but could not find answere");
+          }
+        }
+      }
+    } else {
+      //this ice is coming from the answerer. Send to the offerer
+      //pass it through to the other socket
+      const offerInOffers = offers.find(
+        (o) => o.answererUserName === iceUserName
+      );
+      const socketToSendTo = connectedSockets.find(
+        (s) => s.userName === offerInOffers.offererUserName
+      );
+      if (socketToSendTo) {
+        socket
+          .to(socketToSendTo.socketId)
+          .emit("receivedIceCandidateFromServer", iceCandidate);
+      } else {
+        console.log("Ice candidate recieved but could not find offerer");
+      }
+    }
+    // console.log(offers)
+  });
+});
 
 mongodb.connect(process.env.DATABASE_URL);
 const db = mongodb.connection;
